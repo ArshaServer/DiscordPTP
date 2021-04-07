0197e3caa841d2867a2929d28b05dea1e73f372a
Madafi <46978886+ArshaServer@users.noreply.github.com>
2021-04-08 00:29:50 +0200

    TicTacToe Fix

--- Games/TicTacToe.py
+++ Games/TicTacToe.py
@@ -15,48 +15,38 @@ class TicTacToe():
     async def next_turn(self, message):
         args = message.content.split(' ')
         if len(args) == 2:
-            if(self.Result(self.feld) == False):
-                self.SpielfeldChange(self.feld, int(args[1]), self.anzahlZuege)
+            if(self.Result() == False):
+                self.SpielfeldChange((int)(args[1]))
                 self.anzahlZuege+=1
                 embed = discord.Embed(title="TicTacTo",color=0x22a7f0)
-                embed.add_field(name="Feld:",value=self.Spielfeld(self.feld))
+                embed.add_field(name="Feld:",value=self.Spielfeld())
                 await message.channel.send(embed=embed)
-                if(self.Result(self.feld) == True):
-                    gewonnenMessage = await message.channel.send("Hast Gewonnen {0}".format(self.Spieler(self.anzahlZuege-1)))
+                if(self.Result() == True):
+                    gewonnenMessage = await message.channel.send("Hast Gewonnen {0}".format(self.Spieler()))
                     await gewonnenMessage.add_reaction("<:GayAlex:642807888280027147>") 
                     await message.channel.send("zum Neustarten !start TicTacTo schreiben")
-                if(self.Unentschieden(self.feld) == True):
+                if(self.Unentschieden() == True):
                     await message.channel.send("Unentschieden")
                     await message.channel.send("zum Neustarten !start TicTacTo schreiben") 
 
     def Spielfeld(self):
         spielfeld = ("{0} {1} {2} \n{3} {4} {5}\n{6} {7} {8}".format(
-            self.feld[0][0],
-            self.feld[0][1],
-            self.feld[0][2],
-            self.feld[1][0],
-            self.feld[1][1],
-            self.feld[1][2],
-            self.feld[2][0],
-            self.feld[2][1],
-            self.feld[2][2])
+            self.feld[0][0],self.feld[0][1],self.feld[0][2],
+            self.feld[1][0],self.feld[1][1],self.feld[1][2],
+            self.feld[2][0],self.feld[2][1],self.feld[2][2])
         )
         return spielfeld
 
-    def SpielfeldChange(self, spielfeld, eingabefeld):
-        raus = False
-        for i in range(len(spielfeld)):
-            if(raus == True):
-                break
-            for j in range(len(spielfeld[i])):
-                eingabefeld-=1
-                if(eingabefeld == 0):
-                    if(spielfeld[i][j] != 'X' and spielfeld[i][j] != 'O'):
-                        spielfeld[i][j] = self.Spieler(self.anzahlZuege)
-                        raus = True
-                        break
+    def SpielfeldChange(self, eingabe):
+        if(eingabe%3 == 0):
+            i = (int)(eingabe/3-1)
+        else:
+            i = (int)(eingabe/3)
+        if(self.feld[i][(int)(eingabe%3)-1] != 'X' and self.feld[i][(int)(eingabe%3)-1] != 'O'):
+            self.feld[i][(int)(eingabe%3)-1] = self.Spieler()
+            return
 
-    def GleicheZeichen(eins, zwei, drei):
+    def GleicheZeichen(self, eins, zwei, drei):
         alleGleich = False
         if(eins == 'X'):
             if(zwei == 'X'):
@@ -82,22 +72,22 @@ class TicTacToe():
     def Result(self):
         Gewonnen = False
         for i in range(3):
-            if(self.GleicheZeichen(self.feld[i][0],self.feld[i][1],self.feld[i][2]) == True):
+            if(self.GleicheZeichen(self.feld[0][i],self.feld[1][i],self.feld[2][i])):
                 Gewonnen = True
                 break
-            elif(self.GleicheZeichen(self.feld[0][i],self.feld[1][i],self.feld[2][i]) == True):
+            elif(self.GleicheZeichen(self.feld[i][0],self.feld[i][1],self.feld[i][2])):
                 Gewonnen = True
                 break
-        if(self.GleicheZeichen(self.feld[0][0],self.feld[1][1],self.feld[2][2]) == True):
+        if(self.GleicheZeichen(self.feld[0][0],self.feld[1][1],self.feld[2][2])):
             Gewonnen = True      
-        elif(self.GleicheZeichen(self.feld[0][2],self.feld[1][1],self.feld[2][0]) == True):
+        elif(self.GleicheZeichen(self.feld[0][2],self.feld[1][1],self.feld[2][0])):
             Gewonnen = True
         return Gewonnen
 
     def Spieler(self):
         XO = 'X'
-        if(self.anzahlZuege == 0 or self.anzahlZuege == 2 or self.anzahlZuege == 4 or self.anzahlZuege == 6 or self.anzahlZuege == 8):
+        if(self.anzahlZuege%2==0):
             XO = 'X'    
-        elif(self.anzahlZuege == 1 or self.anzahlZuege == 3 or self.anzahlZuege == 5 or self.anzahlZuege == 7 or self.anzahlZuege == 9):
+        else:
             XO = 'O'
         return XO
