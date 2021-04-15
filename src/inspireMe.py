3ea51b0935ad956ad397fae28bdfeb8a7eb13086
TreeBoiGreg <treeboiGreg@gmail.com>
2021-04-15 21:21:46 +0200

    merge conflict fix

--- src/inspireMe.py
+++ src/inspireMe.py
@@ -76,7 +76,7 @@ def add_autism(str):
     d = ImageDraw.Draw(out)
 
     # draw multiline text
-    d.multiline_text((50,550),wrap_text(autism_text(str),20,fnt) , font=fnt, fill=(255, 255, 255))
+    d.multiline_text((50,450),wrap_text(autism_text(str),20,fnt) , font=fnt, fill=(255, 255, 255))
 
     out.save("src/data/autism.png","png")
 
@@ -89,8 +89,8 @@ async def getImage(message):
         # create an image
         out = Image.new("RGB", (700, 500), (255, 10, 70))
         # get a font
-        fnt1 = ImageFont.truetype("data/Chalkboard.ttc", 20)
-        fnt2 = ImageFont.truetype("data/Zapfino.ttf", 20)
+        fnt1 = ImageFont.truetype("src/data/Chalkboard.ttc", 20)
+        fnt2 = ImageFont.truetype("src/data/Zapfino.ttf", 20)
         # get a drawing context
         d = ImageDraw.Draw(out)
 
@@ -98,8 +98,8 @@ async def getImage(message):
         d.multiline_text((10,10), "Ein {0} sagte eins:".format(message.author.name), font=fnt1, fill=(0, 0, 0))
         d.multiline_text((10,50), wrap_text(args[2],600,fnt2), font=fnt2, fill=(0, 0, 0))
         
-        out.save("data/img.png","png")
-        with open('data/img.png', 'rb') as f:
+        out.save("src/data/img.png","png")
+        with open('src/data/img.png', 'rb') as f:
             picture = File(f)
             await message.guild.get_member_named(args[1]).send(file=picture)
         await message.add_reaction("\U00002705")
